c813d0fef0473b9252fc3e3053d724878bdc2c3c
Ang3licaValdo <angelica.gonzalez.valdovinos2@gmail.com>
2023-02-18 00:38:57 +0100

    nueva

--- cloud.py
+++ cloud.py
@@ -7,7 +7,7 @@ def verifyXML(XMLName):
     if '.xml' in XMLName:
         return 1
     else:
-        return "Not an XML file"
+        return "not an XML file"
 
 def openXML(xmlName):
     if verifyXML(xmlName) == 1:
