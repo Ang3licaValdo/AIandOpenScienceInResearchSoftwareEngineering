e520a3757e6e995f69eab795c518752a1b51cd3e
Ang3licaValdo <108503546+Ang3licaValdo@users.noreply.github.com>
2023-02-28 03:18:47 +0100

    Add files via upload

---
+++ 2ndAssigment/request_grobid.py
@@ -0,0 +1,11 @@
+from grobid_client.grobid_client import GrobidClient
+import identify_xml
+import shutil
+import os
+
+#downloading the XML files from GROBID
+client = GrobidClient(config_path="./config.json")
+client.process("processFulltextDocument", "./Papers", n=10)
+
+#Getting the xml
+copy_xml = identify_xml.identifying_xml("./Papers")
