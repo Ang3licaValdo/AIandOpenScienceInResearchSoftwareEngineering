7349baa2bfe8e73bfdf0b1a9dc43fbfca05b6b5b
Ang3licaValdo <108503546+Ang3licaValdo@users.noreply.github.com>
2023-02-28 10:08:40 +0100

    Update request_grobid.py

--- 2ndAssigment/request_grobid.py
+++ 2ndAssigment/request_grobid.py
@@ -5,7 +5,7 @@ import os
 
 #downloading the XML files from GROBID
 client = GrobidClient(config_path="./config.json")
-client.process("processFulltextDocument", "./Papers", n=10)
+client.process("processFulltextDocument", "./Papers", n=2)
 
 #Getting the xml
 copy_xml = identify_xml.identifying_xml("./Papers")
